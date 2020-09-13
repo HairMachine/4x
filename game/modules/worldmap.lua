519f61e33e2af419aeb4d107f667c43e4e04345e
HairMachine <hair.machine@gmail.com>
2020-09-13 13:46:53 +0100

    Surrounded dark tiles turn to light

--- game/modules/worldmap.lua
+++ game/modules/worldmap.lua
@@ -21,6 +21,83 @@ local function tileAlignmentChange()
             end
         end
     end
+    -- Check for enclosed areas
+    -- First, find all the "free" dark tiles - these are the ones that don't have lighted tiles on 3 of their orthogonal directions
+    local freemap = {}
+    for y = 1, MAPSIZEY do
+        freemap[y] = {}
+        for x = 1, MAPSIZEX do
+            -- Lighted tiles are ALWAYS unfree
+            if map[y][x].align == 1 then
+                freemap[y][x] = false
+            else
+                local surroundX = 0
+                local surroundY = 0
+                for n = 1, y do
+                    if map[n][x].align == 1 then 
+                        surroundY = surroundY + 1 
+                        break
+                    end
+                end
+                for e = x, MAPSIZEX do
+                    if map[y][e].align == 1 then 
+                        surroundX = surroundX + 1
+                        break
+                    end
+                end
+                for s = y, MAPSIZEY do
+                    if map[s][x].align == 1 then 
+                        surroundY = surroundY + 1 
+                        break
+                    end
+                end
+                for w = 1, x do
+                    if map[y][w].align == 1 then 
+                        surroundX = surroundX + 1 
+                        break
+                    end
+                end
+                if surroundX > 0 and surroundY > 0 then
+                    freemap[y][x] = false
+                else
+                    freemap[y][x] = true
+                end
+            end
+        end
+    end
+    -- Then, find all the unfree tiles that are connected to a free tile, and mark them as free
+    local changed = true
+    while (changed) do
+        changed = false
+        for y = 1, MAPSIZEY do
+            for x = 1, MAPSIZEX do
+                if freemap[y][x] == false and map[y][x].align == 2 then
+                    if y - 1 >= 1 and freemap[y - 1][x] == true then 
+                        freemap[y][x] = true
+                        changed = true
+                    end
+                    if x + 1 <= MAPSIZEX and freemap[y][x + 1] == true then 
+                        freemap[y][x] = true 
+                        changed = true
+                    end
+                    if y + 1 <= MAPSIZEY and freemap[y + 1][x] == true then 
+                        freemap[y][x] = true
+                        changed = true
+                    end
+                    if x - 1 >= 1 and freemap[y][x - 1] == true then 
+                        freemap[y][x] = true
+                        changed = true
+                    end
+                end
+            end
+        end
+    end
+    -- All the remaining unfree tiles should now be lighted
+    for y = 1, MAPSIZEY do
+        for x = 1, MAPSIZEX do
+            if freemap[y][x] == false then map[y][x].align = 1 end
+        end
+    end
 end
 
 return {
